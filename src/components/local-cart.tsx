2edfd802768d936ebaaa85806b256d2a23227422
Quentin <Bleeky@users.noreply.github.com>
2024-07-26 11:06:28 +0200

    Update local-cart.tsx

--- src/components/local-cart.tsx
+++ src/components/local-cart.tsx
@@ -92,9 +92,9 @@ export const LocalCart = () => {
             {total === 0 ? (
               <div>Your cart is empty</div>
             ) : (
-              <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
+              <div className="mt-8 flex justify-end border-t border-teal-100 pt-8">
                 <div className="w-screen max-w-lg space-y-4">
-                  <dl className="space-y-0.5 text-sm text-gray-700">
+                  <dl className="space-y-0.5 text-sm text-teal-700">
                     <div className="flex justify-between !text-base font-medium">
                       <dt>Total</dt>
                       <dd>{total}</dd>
@@ -104,7 +104,7 @@ export const LocalCart = () => {
                   <div className="flex justify-end">
                     <a
                       href="/checkout"
-                      className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
+                      className="block rounded bg-teal-600 px-5 py-3 text-sm text-white transition hover:bg-gray-600"
                     >
                       Checkout
                     </a>
